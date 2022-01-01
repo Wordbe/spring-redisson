9d0088f8f183ad772c40c4cbd97c853159e9cf6f
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-01-01 19:37:23 +0900

    feat: key value number test

---
+++ src/test/java/co/whitetree/springredisson/test/NumberTest.java
@@ -0,0 +1,24 @@
+package co.whitetree.springredisson.test;
+
+import co.whitetree.springredisson.test.config.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RAtomicLongReactive;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class NumberTest extends BaseTest {
+    @Test
+    public void keyValueIncrease() {
+        RAtomicLongReactive atomicLong = client.getAtomicLong("user:1:visit");
+        // 자바의 atomicLong 과 비슷
+        Mono<Void> mono = Flux.range(1, 30)
+                .delayElements(Duration.ofSeconds(1))
+                .flatMap(i -> atomicLong.incrementAndGet())
+                .then();
+        StepVerifier.create(mono)
+                .verifyComplete();
+    }
+}
