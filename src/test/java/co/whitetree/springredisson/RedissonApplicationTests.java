7c88518d5fe9a7959930511ae6840ea6efdcdc04
Wordbe <seonghojin3@gmail.com>
2022-05-25 16:48:45 +0900

    redisson better than spring data redis

--- src/test/java/co/whitetree/springredisson/RedissonApplicationTests.java
+++ src/test/java/co/whitetree/springredisson/RedissonApplicationTests.java
@@ -1,13 +1,61 @@
 package co.whitetree.springredisson;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.redisson.api.RAtomicLongReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class RedissonApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private ReactiveStringRedisTemplate template;
+
+    @Autowired
+    private RedissonReactiveClient client;
+
+    /**
+     * Spring Data Reactive Redis Issue
+     * - Performance Issues
+     * - No support for Reactive CRUD repository
+     * - Some annotations do not work with reactive type
+     */
+    @RepeatedTest(3)
+    void springDataRedisTest() {
+        ReactiveValueOperations<String, String> operations = template.opsForValue();
+
+        long start = System.currentTimeMillis();
+
+        Mono<Void> mono = Flux.range(1, 500_000)
+                .flatMap(i -> operations.increment("user:1:visit")) // incr
+                .then();
+        StepVerifier.create(mono)
+                .verifyComplete();
+
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + " ms");
+    }
+
+    @RepeatedTest(3)
+    void redissonTest() {
+        RAtomicLongReactive atomicLong = client.getAtomicLong("user:2:visit");
+
+        long start = System.currentTimeMillis();
+
+        Mono<Void> mono = Flux.range(1, 500_000)
+                .flatMap(i -> atomicLong.incrementAndGet()) // incre
+                .then();
+        StepVerifier.create(mono)
+                .verifyComplete();
+
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + " ms");
     }
 
 }
